7266d5588fc50ecc98256d8bcee3922d43adcc2d
Mahim Haque <mahimanzum@gmail.com>
2024-05-28 19:48:20 -0400

    new code editor page added.

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,44 @@ import '@aws-amplify/ui-react/styles.css';
 import Home from './Home';
 import Blog from './Blog';
 import Todo from './Todo';
+import CodeEditor from './CodeEditor';
+import { Container, AppBar, Toolbar, Button, Typography } from '@mui/material';
 
 const App: React.FC = () => {
     return (
         <Authenticator>
             {({ signOut, user }) => (
                 <Router>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/todo">Todo</Link></li>
-                            <li><Link to="/blog">Blog</Link></li>
-                        </ul>
-                        <button onClick={signOut}>Sign out</button>
-                    </nav>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/todo" element={<Todo />} />
-                        <Route path="/blog" element={<Blog />} />
-                    </Routes>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                My App
+                            </Typography>
+                            <Button color="inherit" component={Link} to="/">
+                                Home
+                            </Button>
+                            <Button color="inherit" component={Link} to="/todo">
+                                Todo
+                            </Button>
+                            <Button color="inherit" component={Link} to="/blog">
+                                Blog
+                            </Button>
+                            <Button color="inherit" component={Link} to="/code-editor">
+                                Code Editor
+                            </Button>
+                            <Button color="inherit" onClick={signOut}>
+                                Sign Out
+                            </Button>
+                        </Toolbar>
+                    </AppBar>
+                    <Container>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/todo" element={<Todo user={user} />} />
+                            <Route path="/blog" element={<Blog />} />
+                            <Route path="/code-editor" element={<CodeEditor />} />
+                        </Routes>
+                    </Container>
                 </Router>
             )}
         </Authenticator>
