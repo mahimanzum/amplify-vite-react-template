6da1881453a3449e1df327933b2f1ae75c0565d7
Mahim Haque <mahimanzum@gmail.com>
2024-05-28 19:18:57 -0400

    new pages added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,33 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import Home from './Home';
+import Blog from './Blog';
+import Todo from './Todo';
 
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
-
-const client = generateClient<Schema>();
-  
-function deleteTodo(id: string) {
-  client.models.Todo.delete({ id })
-}
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  return (
-        
-    <Authenticator>
-        {({ signOut, user }) => (
-        <main>
-          <h1>{user?.signInDetails?.loginId}'s todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map(todo => <li
-              onClick={() => deleteTodo(todo.id)}
-              key={todo.id}>
-              {todo.content}
-            </li>)}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-        
-      )}
-      </Authenticator>
-  )
-}
+const App: React.FC = () => {
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <Router>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/todo">Todo</Link></li>
+                            <li><Link to="/blog">Blog</Link></li>
+                        </ul>
+                        <button onClick={signOut}>Sign out</button>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/todo" element={<Todo />} />
+                        <Route path="/blog" element={<Blog />} />
+                    </Routes>
+                </Router>
+            )}
+        </Authenticator>
+    );
+};
 
 export default App;
